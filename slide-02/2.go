e8ba83064dc70324db8b9eb521ff92ccd320931f
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2019-10-28 22:23:53 +0000

    Update slide 2 code

--- slide-02/2.go
+++ slide-02/2.go
@@ -13,16 +13,14 @@ const slidenumber = "2"
 
 func init() {
 	log.Print("starting init function")
-	//myname = "Boaty McBoatface"
+	myname = "Boaty McBoatface"
 }
 
 func main() {
 	log.Print("main is starting")
 	if len(myname) == 0 {
 		log.Fatal("oops, name is too short! exiting")
-		//os.Exit(1)
 	}
-
 	fmt.Println("my name is:", myname)
 	fmt.Println("MY NAME IS:", myfirstfunction(myname))
 	os.Exit(0)
